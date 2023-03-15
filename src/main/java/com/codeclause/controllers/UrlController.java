8ca2ad165054d0e5554e2ab9a52273f25ba294fc
spiderocta <muhammadahmed@tutanota.com>
2023-03-15 09:20:02 +0200

    url controller

---
+++ src/main/java/com/codeclause/controllers/UrlController.java
@@ -0,0 +1,34 @@
+package com.codeclause.controllers;
+
+import com.codeclause.dtos.LongUrl;
+import com.codeclause.services.UrlService;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/")
+public class UrlController {
+    private final UrlService urlService;
+
+    public UrlController(UrlService urlService) {
+        this.urlService = urlService;
+    }
+
+    @PostMapping("shorten")
+    public String convertToShortUrl(@RequestBody LongUrl url) {
+        return urlService.convertToShortUrl(url);
+    }
+
+    @GetMapping(value = "{shortUrl}")
+    @Cacheable(value = "urls", key = "#shortUrl", sync = true)
+    public ResponseEntity<Void> getAndRedirect(@PathVariable String shortUrl) {
+        var url = urlService.getOriginalUrl(shortUrl);
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(URI.create(url))
+                .build();
+    }
+}
