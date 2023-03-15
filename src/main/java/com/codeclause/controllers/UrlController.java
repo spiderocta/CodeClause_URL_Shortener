80d4789c17626806bfac6cbf619a3c1009d40474
spiderocta <muhammadahmed@tutanota.com>
2023-03-15 09:56:27 +0200

    url controller mod

--- src/main/java/com/codeclause/controllers/UrlController.java
+++ src/main/java/com/codeclause/controllers/UrlController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/v1")
 public class UrlController {
     private final UrlService urlService;
 
